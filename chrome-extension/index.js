9d5d8182e128f546ff2d87c373563b746d333aee
levy kipkoech <kipkoechlevy096@gmail.com>
2024-04-21 21:39:08 +0300

    added a save button

---
+++ chrome-extension/index.js
@@ -0,0 +1,6 @@
+// Log out "Button clicked!" when the user clicks the "SAVE INPUT" button
+//let button = document.getElementById("input-btn")
+
+function saveButton(){
+    console.log("button clicked")
+}
